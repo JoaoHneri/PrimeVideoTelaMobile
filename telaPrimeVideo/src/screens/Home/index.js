7d56edf800cf81c21de19e96da06a5cd7a58d93b
João Henrique <joaohnsousa@gmail.com>
2024-02-16 00:42:25 -0300

    Criação de Header do aplicativo

--- telaPrimeVideo/src/screens/Home/index.js
+++ telaPrimeVideo/src/screens/Home/index.js
@@ -1,7 +1,65 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
+import primeVideoLogo from "../../assets/prime_video.png";
+
+import AmazonLogo from "../../assets/amazon_logo.png";
+
+import SerieTheWheel from "../../assets/movies/the_wheel_of_time.png";
 export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.primeLogoImg} source={primeVideoLogo}/>
+        <Image style={styles.amazonLogoImg} source={AmazonLogo}/>
 
-    return <Text>Home</Text>
-    
-}
+        <View style={styles.category}>
+            <TouchableOpacity>
+                <Text style={styles.categoryText}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style={styles.categoryText}>TV Shows</Text>
+            </TouchableOpacity><TouchableOpacity>
+                <Text style={styles.categoryText}>Movies</Text>
+            </TouchableOpacity><TouchableOpacity>
+                <Text style={styles.categoryText}>Kids</Text>
+            </TouchableOpacity>
+        </View>
+        <TouchableOpacity>
+                <Image source={SerieTheWheel}/>
+            </TouchableOpacity>
+      </View>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#232F3E",
+    alignItems: "flex-start"
+  },
+  header: {
+    width: "100%",
+    paddingTop: 80,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  amazonLogoImg: {
+    marginTop: -32,
+    marginLeft: 30,
+  },
+  primeLogoImg: {},
+  category: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 30,
+    marginBottom: 15,
+  },
+  categoryText:{
+    fontSize: 14,
+    fontWeight: "700",
+    color: "#fff",
+  }
+});
